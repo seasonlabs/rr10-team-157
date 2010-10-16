029247cd1e73e9f5cab9cb7d67f00aa0cd1dbc07
Victor Castell <victorcoder@gmail.com>
2010-10-16 20:07:03 +0200

    Partials bug

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,4 +1,10 @@
 class IndexController < ApplicationController
+  before_filter :redirect_links
+  
+  def redirect_links
+    redirect_to links_url if signed_in?
+  end
+  
   def index
     render :layout => 'home'
   end
