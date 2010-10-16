04c7f02b0a53136778a8267e94351526e5e96cde
Victor Castell <victorcoder@gmail.com>
2010-10-16 10:48:22 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Linkodrama::Application.config.secret_token = 'df5ce31b4618901c04aa561829849405fa2fa42ed48fc87e563bebfbea44a6687b986383567fc2db5d389fe3be8ee5631041678def786cdec6bd38bc2e09caa2'
