f6691734a4a14ed118f3965ddb3ee104b04a9dc8
Victor Castell <victorcoder@gmail.com>
2010-10-16 13:58:30 +0200

    Authentications system ready

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,20 @@ class SessionsController < ApplicationController
       # whether there is already a user signed in.
       @auth = Authorization.create_from_hash(auth, current_user)
     end
+    
     # Log the authorizing user in.
     self.current_user = @auth.user
   
-    redirect_to :index
+    redirect_to root_url
+  end
+  
+  def destroy
+    reset_session
+    flash[:notice] = "You've been signed out"
+    redirect_to root_url
+  end
+  
+  def failure
+    render :text => "Authentication Failure!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ Linkodrama::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "index#index"
 
   # See how all your routes lay out with "rake routes"
 
@@ -56,4 +56,7 @@ Linkodrama::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id(.:format)))'
   match '/auth/:provider/callback', :to => 'sessions#create'
+  match '/auth/failure', :to => 'sessions#failure'
+  
+  match '/sessions/destroy', :to => 'sessions#destroy'
 end
