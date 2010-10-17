8b0bb18ad09e0beabf0c31e9d8e6eb89f28a1a5b
Victor Castell <victorcoder@gmail.com>
2010-10-17 22:07:28 +0200

    Encoding

--- config/environment.rb
+++ config/environment.rb
@@ -4,7 +4,7 @@ require File.expand_path('../application', __FILE__)
 # load fb info
 APP_CONFIG = YAML::load_file("#{Rails.root}/config/twitter.yml")
 
-require 'lib/mysql_utf8'
+require 'lib/mysql_utf8.rb'
 
 # Initialize the rails application
 Linkodrama::Application.initialize!
