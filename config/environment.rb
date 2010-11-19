697746c25737b24c4242d8e22333d8f0c50e2b6f
Victor Castell <victorcoder@gmail.com>
2010-11-19 17:39:34 +0100

    Uninstalled ex notifier

--- config/environment.rb
+++ config/environment.rb
@@ -6,10 +6,5 @@ APP_CONFIG = YAML::load_file("#{Rails.root}/config/twitter.yml")
 
 require File.dirname(__FILE__) + '/../lib/mysql_utf8'
 
-Linkodrama::Application.config.middleware.use ExceptionNotifier,
-   :email_prefix => "[linkodrama] ",
-   :sender_address => %{"notifier" <app@linkodrama.com>},
-   :exception_recipients => %w{info@season.es}
-
 # Initialize the rails application
 Linkodrama::Application.initialize!
